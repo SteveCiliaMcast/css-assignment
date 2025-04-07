26edc26845409d8f4c49d4a08783445e1f7b74f7
Steve Cilia <steve.cilia.f32203@mcast.edu.mt>
2025-04-07 18:19:18 +0200

    add navbar component and routing setup for login and client management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ClientListComponent } from './components/client-list/client-list.component';
+import { ClientFormComponent } from './components/client-form/client-form.component';
+import { ClientDetailComponent } from './components/client-detail/client-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'clients', component: ClientListComponent },
+  { path: 'clients/:id', component: ClientDetailComponent },
+  { path: 'add-client', component: ClientFormComponent },
+  { path: 'clients/:id/edit', component: ClientFormComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
